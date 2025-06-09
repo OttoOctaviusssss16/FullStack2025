6e155895e9118d7d5c82a3cda7e58bcb1ce13e4e
octaviodev <anonymousgamerpack@gmail.com>
2025-06-09 08:46:14 -0300

    a

--- ProyectoPhp/Presentacion/panelLogin.php
+++ ProyectoPhp/Presentacion/panelLogin.php
@@ -7,13 +7,14 @@
     <link rel="stylesheet" href="../Estilos/EstiloLogin.css">
 </head>
 <body>
+        <header>Hola</header>
         <form class="form_login_registro" action="" method="post">
-        <label class="login_label">Email</label><input type="text" name="email">
+        <label class="login_label">Email</label><input type="text" name="email" placeholder="Ingrese su Email" class="ingresotexto">
         <br>
-        <label class="login_label">Contraseña</label><input type="password" name="contraseña">
+        <label class="login_label">Contraseña</label><input type="password" name="contraseña" placeholder="Ingrese su contraseña" class="ingresotexto">
         <br>
-        <input type="submit" name="login" value="Iniciar Sesión">
-        <input type="submit" name="register" value="Registro">
+        <input type="submit" name="login" value="Iniciar Sesión" class="botonestandar">
+        <input type="submit" name="register" value="Registro" class="botonestandar">
     </form>
 </body>
 </html>
@@ -37,10 +38,18 @@ $usuario2->setEmail("peru@peruano.com");
 $usuario2->setContraseña("peruano");
 $clientes=[$usuario1, $usuario2];
 if(isset($_POST['login'])){
-$cliente = new cliente();
-$cliente->setNombre($_POST['email']);
-$cliente->setNombre($_POST['contraseña']);
+if ($usuario1->getEmail() == $_POST['email'] && $usuario1->getContraseña()== $_POST['contraseña'] ){
+        if ($usuario1->getTipo() == "admin"){
+        header("Location:panelAdmin.php");
+    }else{
+    header("Location:index.php");
 }
+}else{
+    echo "
+    <script>
+    alert('Usuario o Contraseña Incorrectos');
+    </script>";
+}}
 if(isset($_POST['register'])){
 header("Location:panelRegistro.php");
 }
